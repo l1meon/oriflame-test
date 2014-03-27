cc897000f3b9958069109e0301b3befd7e4a5d86
Dumitru Railean <bratuwka93@gmail.com>
2014-03-27 02:17:58 +0200

    change admin user && add seed data

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Oriflame
      config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
      config.i18n.default_locale = :ru
      config.assets.enabled = true
+     config.serve_static_assets = true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+category = Category.create([{id: 1,name: 'для лица', name_ro: 'piele'},{id: 2,name: 'макияж', name_ro: 'machiaj'},{id: 3,name: 'парфюмерия', name_ro: 'parfumuri'},
+                            {id: 4,name: 'для тела', name_ro: 'corp'},
+                            {id: 5,name: 'для волос', name_ro: 'par'},{id: 6,name: 'акссесуары', name_ro: 'accesorii'},
+                            {id: 7,name: 'для мужчин', name_ro: 'pentru barbati'},{id: 8,name: 'very me', name_ro: 'very me'},{id: 9,name: 'wellness', name_ro: 'wellness'}])
